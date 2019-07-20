59af34f7c6425d59e1ec43233bc664c369fe3db3
mark146 <tkdtkdqja@naver.com>
2019-07-20 22:55:55 +0900

    Update example_02.sql

--- example_02.sql
+++ example_02.sql
@@ -1,102 +1,36 @@
-Rem Copyright (c) 1990 by Oracle Corporation
-Rem NAME
-REM    UTLSAMPL.SQL
-Rem  FUNCTION
-Rem  NOTES
-Rem  MODIFIED
-Rem	gdudey	   06/28/95 -  Modified for desktop seed database
-Rem	glumpkin   10/21/92 -  Renamed from SQLBLD.SQL
-Rem	blinden   07/27/92 -  Added primary and foreign keys to EMP and DEPT
-Rem	rlim	   04/29/91 -	      change char to varchar2
-Rem	mmoore	   04/08/91 -	      use unlimited tablespace priv
-Rem	pritto	   04/04/91 -	      change SYSDATE to 13-JUL-87
-Rem   Mendels	 12/07/90 - bug 30123;add to_date calls so language independent
-Rem
-rem
-rem $Header: utlsampl.sql 7020100.1 94/09/23 22:14:24 cli Generic<base> $ sqlbld.sql
-rem
-SET TERMOUT OFF
-SET ECHO OFF
+<-- 테스트 샘플 생성 -->
+create table t 
+as select d.no, e.* from scott.emp e
+,(select rownum no from dual connect by level <= 1000) d;
 
-rem CONGDON    Invoked in RDBMS at build time.	 29-DEC-1988
-rem OATES:     Created: 16-Feb-83
+create index t_x01 on t(deptno, no);
+create index t_x02 on t(deptno, job, no);
+
+<-- t 테이블에 통계정보를 수집하는 쿼리 -->
+exec dbms_stats.gather_table_stats(user, 't');
 
 
+select * from t
+where deptno = 10
+and no =1;
 
+<-- 힌트 사용법 -->
+select /*+ index(t t_x02) */ * from t
+where deptno = 10
+and no = 1;
 
-GRANT CONNECT,RESOURCE,UNLIMITED TABLESPACE TO SCOTT IDENTIFIED BY TIGER;
-ALTER USER SCOTT DEFAULT TABLESPACE USERS;
-ALTER USER SCOTT TEMPORARY TABLESPACE TEMP;
-CONNECT SCOTT/tiger
-DROP TABLE DEPT;
-CREATE TABLE DEPT
-       (DEPTNO NUMBER(2) CONSTRAINT PK_DEPT PRIMARY KEY,
-	DNAME VARCHAR2(14) ,
-	LOC VARCHAR2(13) ) ;
-DROP TABLE EMP;
-CREATE TABLE EMP
-       (EMPNO NUMBER(4) CONSTRAINT PK_EMP PRIMARY KEY,
-	ENAME VARCHAR2(10),
-	JOB VARCHAR2(9),
-	MGR NUMBER(4),
-	HIREDATE DATE,
-	SAL NUMBER(7,2),
-	COMM NUMBER(7,2),
-	DEPTNO NUMBER(2) CONSTRAINT FK_DEPTNO REFERENCES DEPT);
-INSERT INTO DEPT VALUES
-	(10,'ACCOUNTING','NEW YORK');
-INSERT INTO DEPT VALUES (20,'RESEARCH','DALLAS');
-INSERT INTO DEPT VALUES
-	(30,'SALES','CHICAGO');
-INSERT INTO DEPT VALUES
-	(40,'OPERATIONS','BOSTON');
-INSERT INTO EMP VALUES
-(7369,'SMITH','CLERK',7902,to_date('17-12-1980','dd-mm-yyyy'),800,NULL,20);
-INSERT INTO EMP VALUES
-(7499,'ALLEN','SALESMAN',7698,to_date('20-2-1981','dd-mm-yyyy'),1600,300,30);
-INSERT INTO EMP VALUES
-(7521,'WARD','SALESMAN',7698,to_date('22-2-1981','dd-mm-yyyy'),1250,500,30);
-INSERT INTO EMP VALUES
-(7566,'JONES','MANAGER',7839,to_date('2-4-1981','dd-mm-yyyy'),2975,NULL,20);
-INSERT INTO EMP VALUES
-(7654,'MARTIN','SALESMAN',7698,to_date('28-9-1981','dd-mm-yyyy'),1250,1400,30);
-INSERT INTO EMP VALUES
-(7698,'BLAKE','MANAGER',7839,to_date('1-5-1981','dd-mm-yyyy'),2850,NULL,30);
-INSERT INTO EMP VALUES
-(7782,'CLARK','MANAGER',7839,to_date('9-6-1981','dd-mm-yyyy'),2450,NULL,10);
-INSERT INTO EMP VALUES
-(7788,'SCOTT','ANALYST',7566,to_date('13-JUL-87')-85,3000,NULL,20);
-INSERT INTO EMP VALUES
-(7839,'KING','PRESIDENT',NULL,to_date('17-11-1981','dd-mm-yyyy'),5000,NULL,10);
-INSERT INTO EMP VALUES
-(7844,'TURNER','SALESMAN',7698,to_date('8-9-1981','dd-mm-yyyy'),1500,0,30);
-INSERT INTO EMP VALUES
-(7876,'ADAMS','CLERK',7788,to_date('13-JUL-87')-51,1100,NULL,20);
-INSERT INTO EMP VALUES
-(7900,'JAMES','CLERK',7698,to_date('3-12-1981','dd-mm-yyyy'),950,NULL,30);
-INSERT INTO EMP VALUES
-(7902,'FORD','ANALYST',7566,to_date('3-12-1981','dd-mm-yyyy'),3000,NULL,20);
-INSERT INTO EMP VALUES
-(7934,'MILLER','CLERK',7782,to_date('23-1-1982','dd-mm-yyyy'),1300,NULL,10);
-DROP TABLE BONUS;
-CREATE TABLE BONUS
-	(
-	ENAME VARCHAR2(10)	,
-	JOB VARCHAR2(9)  ,
-	SAL NUMBER,
-	COMM NUMBER
-	) ;
-DROP TABLE SALGRADE;
-CREATE TABLE SALGRADE
-      ( GRADE NUMBER,
-	LOSAL NUMBER,
-	HISAL NUMBER );
-INSERT INTO SALGRADE VALUES (1,700,1200);
-INSERT INTO SALGRADE VALUES (2,1201,1400);
-INSERT INTO SALGRADE VALUES (3,1401,2000);
-INSERT INTO SALGRADE VALUES (4,2001,3000);
-INSERT INTO SALGRADE VALUES (5,3001,9999);
-COMMIT;
+<-- Oracle 세그먼트에 할당된 익스텐트 목록 조회-->
+SELECT SEGMENT_TYPE, TABLESPACE_NAME, EXTENT_ID, FILE_ID, BLOCK_ID, BLOCKS
+FROM DBA_EXTENTS
+WHERE OWNER = USER
+AND SEGMENT_NAME = 'MY_SEGMENT'
+ORDER BY EXTENT_ID;
 
-SET TERMOUT ON
-SET ECHO ON
+<-- Oracle block size 확인 -->
+SHOW PARAMETER BLOCK_SIZE;
+select value from v$parameter where name='db_block_size';
+
+<-- 버퍼캐시 사이즈 확인 -->
+show sga;
+
+show parameter db_file_multiblock_read_count;
